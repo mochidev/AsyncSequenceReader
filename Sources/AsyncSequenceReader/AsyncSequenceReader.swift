695dc567614087cf1c8195c64b32ceff12da3e3e
Dimitri Bouniol <dimitribouniol@mochidev.com>
2021-11-17 12:45:34 -0800

    Added AsyncSequenceReader

--- Sources/AsyncSequenceReader/AsyncSequenceReader.swift
+++ Sources/AsyncSequenceReader/AsyncSequenceReader.swift
@@ -6,3 +6,62 @@
 //  Copyright © 2021 Mochi Development, Inc. All rights reserved.
 //
 
+#if compiler(>=5.5) && canImport(_Concurrency)
+
+/// An asynchronous sequence that will read from a mutable iterator so long as the specified condition is valid.
+///
+/// When finished, the iterator can be read back to read other values.
+public class AsyncSequenceReader<BaseIterator: AsyncIteratorProtocol>: AsyncReadSequence {
+    /// The baseIterator to read from.
+    ///
+    /// When finished with the sequence, callers should read back this value so they can continue iterating on the sequence.
+    public var baseIterator: AsyncBufferedIterator<BaseIterator>
+    
+    /// The closure to call when a new value is requested.
+    ///
+    /// Note that implementers must **never** read from the iterator if they don't expect to forward the value. It is, however, acceptable to save the last value sent, and verify that before the next read to check if the read sequence should end early (by returning nil) or not. Ending a sequence early is completely supported, allowing more reads within a different context from occuring.
+    @usableFromInline
+    let read: (_ iterator: inout AsyncBufferedIterator<BaseIterator>) async throws -> Element?
+    
+    @usableFromInline
+    init(
+        _ baseIterator: AsyncBufferedIterator<BaseIterator>,
+        read: @escaping (_ iterator: inout AsyncBufferedIterator<BaseIterator>) async throws -> Element?
+    ) {
+        self.baseIterator = baseIterator
+        self.read = read
+    }
+}
+
+extension AsyncSequenceReader: AsyncSequence {
+    /// The type of element produced by this asynchronous sequence.
+    ///
+    /// The read sequence produces whatever type of element its base iterator produces.
+    public typealias Element = BaseIterator.Element
+    
+    /// The iterator that produces elements of the read sequence.
+    public struct AsyncIterator: AsyncIteratorProtocol {
+        @usableFromInline
+        var readSequence: AsyncSequenceReader
+        
+        @usableFromInline
+        init(_ readSequence: AsyncSequenceReader) {
+            self.readSequence = readSequence
+        }
+        
+        /// Produces the next element in the sequence.
+        ///
+        /// This iterator calls `read()` with its base iterator, and lets that closure produce an appropriate result.
+        @inlinable
+        public mutating func next() async throws -> Element? {
+            return try await readSequence.read(&readSequence.baseIterator)
+        }
+    }
+    
+    @inlinable
+    public func makeAsyncIterator() -> AsyncIterator {
+        AsyncIterator(self)
+    }
+}
+
+#endif
--- Tests/AsyncSequenceReaderTests/AsyncSequenceReaderTests.swift
+++ Tests/AsyncSequenceReaderTests/AsyncSequenceReaderTests.swift
@@ -10,7 +10,218 @@ import XCTest
 @testable import AsyncSequenceReader
 
 final class AsyncSequenceReaderTests: XCTestCase {
-    func testExample() throws {
+    // MARK: - Test Manual Iteration
+    
+    func testReadSequenceFromStream() async throws {
+        let testStream = AsyncStream<Int> { continuation in
+            for value in 0..<10 {
+                continuation.yield(value)
+            }
+            continuation.finish()
+        }
         
+        var iterator = AsyncBufferedIterator(testStream.makeAsyncIterator())
+        
+        let readSequenceA = AsyncSequenceReader(iterator) { iterator in
+            let next = await iterator.next()
+            return next
+        }
+        
+        var readSequenceAIterator = readSequenceA.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 0)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 1)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 2)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 3)
+        
+        iterator = readSequenceA.baseIterator
+        
+        var totalRead: Int = 0
+        let readSequenceB = AsyncSequenceReader(iterator) { iterator in
+            guard totalRead < 4 else { return nil }
+            let next = await iterator.next()
+            totalRead += 1
+            return next
+        }
+        
+        var readSequenceBIterator = readSequenceB.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 4)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 5)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 6)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 7)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), nil)
+        
+        iterator = readSequenceB.baseIterator
+        
+        await AsyncXCTAssertEqual(await iterator.next(), 8)
+        await AsyncXCTAssertEqual(await iterator.next(), 9)
+        await AsyncXCTAssertEqual(await iterator.next(), nil)
+    }
+    
+    func testReadSequenceFromTestSequence() async throws {
+        let testStream = TestSequence(base: 0..<10)
+        
+        var iterator = AsyncBufferedIterator(testStream.makeAsyncIterator())
+        
+        let readSequenceA = AsyncSequenceReader(iterator) { iterator in
+            let next = await iterator.next()
+            return next
+        }
+        
+        var readSequenceAIterator = readSequenceA.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 0)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 1)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 2)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 3)
+        
+        iterator = readSequenceA.baseIterator
+        
+        var totalRead: Int = 0
+        let readSequenceB = AsyncSequenceReader(iterator) { iterator in
+            guard totalRead < 4 else { return nil }
+            let next = await iterator.next()
+            totalRead += 1
+            return next
+        }
+        
+        var readSequenceBIterator = readSequenceB.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 4)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 5)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 6)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 7)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), nil)
+        
+        iterator = readSequenceB.baseIterator
+        
+        await AsyncXCTAssertEqual(await iterator.next(), 8)
+        await AsyncXCTAssertEqual(await iterator.next(), 9)
+        await AsyncXCTAssertEqual(await iterator.next(), nil)
+    }
+    
+    func testReadSequenceFromThrowingTestSequence() async throws {
+        let testStream = ThrowingTestSequence(base: 0..<10)
+        
+        var iterator = AsyncBufferedIterator(testStream.makeAsyncIterator())
+        
+        let readSequenceA = AsyncSequenceReader(iterator) { iterator in
+            let next = try await iterator.next()
+            return next
+        }
+        
+        var readSequenceAIterator = readSequenceA.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 0)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 1)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 2)
+        try await AsyncXCTAssertEqual(await readSequenceAIterator.next(), 3)
+        
+        iterator = readSequenceA.baseIterator
+        
+        var totalRead: Int = 0
+        let readSequenceB = AsyncSequenceReader(iterator) { iterator in
+            guard totalRead < 4 else { return nil }
+            let next = try await iterator.next()
+            totalRead += 1
+            return next
+        }
+        
+        var readSequenceBIterator = readSequenceB.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 4)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 5)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 6)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), 7)
+        try await AsyncXCTAssertEqual(await readSequenceBIterator.next(), nil)
+        
+        iterator = readSequenceB.baseIterator
+        
+        try await AsyncXCTAssertEqual(await iterator.next(), 8)
+        try await AsyncXCTAssertEqual(await iterator.next(), 9)
+        try await AsyncXCTAssertEqual(await iterator.next(), nil)
+    }
+    
+    // MARK: - Test Transforms
+    
+    func countCharacters<ReadSequence: AsyncReadSequence>(_ sequence: ReadSequence) async throws -> Int? where ReadSequence.Element == String {
+        try await sequence.map { $0.count }.reduce(into: 0) { partialResult, next in
+            partialResult += next
+        }
+    }
+    
+    func makeReadFourElementSequence<BaseIterator>(_ iterator: inout AsyncBufferedIterator<BaseIterator>) -> AsyncSequenceReader<BaseIterator> {
+        var totalRead: Int = 0
+        return AsyncSequenceReader(iterator) { iterator in
+            guard totalRead < 4 else { return nil }
+            let next = try await iterator.next()
+            totalRead += 1
+            return next
+        }
+    }
+    
+    func testTransformSequenceFromStream() async throws {
+        let testStream = AsyncStream<String> { continuation in
+            let data = ["A", "few", "words", "to", "consider", "today", "as", "this", "test", "runs"]
+            for value in data {
+                continuation.yield(value)
+            }
+            continuation.finish()
+        }
+        
+        var iterator = testStream.makeAsyncIterator()
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 11)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 19)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 8)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), nil)
+        
+        var iteratorB = testStream.makeAsyncIterator()
+        try await AsyncXCTAssertEqual(await iteratorB.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), nil)
+    }
+    
+    func testTransformSequenceFromBufferedStream() async throws {
+        let testStream = AsyncStream<String> { continuation in
+            let data = ["A", "few", "words", "to", "consider", "today", "as", "this", "test", "runs"]
+            for value in data {
+                continuation.yield(value)
+            }
+            continuation.finish()
+        }
+        
+        var iterator = AsyncBufferedIterator(testStream.makeAsyncIterator())
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 11)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 19)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 8)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), nil)
+        
+        var iteratorB = AsyncBufferedIterator(testStream.makeAsyncIterator())
+        try await AsyncXCTAssertEqual(await iteratorB.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), nil)
+    }
+    
+    func testTransformSequenceFromTestSequence() async throws {
+        let testStream = TestSequence(base: ["A", "few", "words", "to", "consider", "today", "as", "this", "test", "runs"])
+        
+        var iterator = testStream.makeAsyncIterator()
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 11)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 19)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 8)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), nil)
+        
+        var iteratorB = testStream.makeAsyncIterator()
+        try await AsyncXCTAssertEqual(await iteratorB.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 11)
+    }
+    
+    func testTransformSequenceFromThrowingTestSequence() async throws {
+        let testStream = ThrowingTestSequence(base: ["A", "few", "words", "to", "consider", "today", "as", "this", "test", "runs"])
+        
+        var iterator = testStream.makeAsyncIterator()
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 11)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 19)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 8)
+        try await AsyncXCTAssertEqual(await iterator.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), nil)
+        
+        var iteratorB = testStream.makeAsyncIterator()
+        try await AsyncXCTAssertEqual(await iteratorB.transform(with: countCharacters, readSequenceFactory: makeReadFourElementSequence), 11)
     }
 }
