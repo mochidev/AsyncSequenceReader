bef63dea03fe43d26ac6252d75cb2a7e8c325319
Dimitri Bouniol <dimitribouniol@mochidev.com>
2021-11-17 01:01:27 -0800

    Added iterator map functionality

---
+++ Sources/AsyncSequenceReader/AsyncIteratorMapSequence.swift
@@ -0,0 +1,210 @@
+//
+//  AsyncIteratorMapSequence.swift
+//  AsyncSequenceReader
+//
+//  Created by Dimitri Bouniol on 2021-11-17.
+//  Copyright © 2021 Mochi Development, Inc. All rights reserved.
+//
+
+#if compiler(>=5.5) && canImport(_Concurrency)
+extension AsyncSequence {
+    
+    /// Creates an asynchronous sequence that maps the given closure over an iterator for the sequence, which can itself accept multiple reads.
+    ///
+    /// When finished reading from the iterator, return your completed object, and the closure will be called again with an terator configured to continue where the first one finished.
+    ///
+    /// In this example, an asynchronous sequence of Strings encodes sentences by prefixing each word sequence with a number.
+    /// The number indicates how many words will be read and concatenated into a complete sentence.
+    ///
+    /// The closure provided to the `iteratorMap(_:)` first reads the first available string, interpreting it as a number.
+    /// Then, it will loop the specified number of times, accumulating those words into an array, that is finally assembled into a sentence.
+    ///
+    ///     let dataStream = ... // "2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "1", "Bye!"
+    ///
+    ///     let sentenceStream = dataStream.iteratorMap { iterator -> String? in
+    ///         var count = Int(try await iterator.next() ?? "")!
+    ///
+    ///         var results: [String] = []
+    ///
+    ///         while count > 0, let next = try await iterator.next() {
+    ///             results.append(next)
+    ///             count -= 1
+    ///         }
+    ///
+    ///         return results.joined(separator: " ")
+    ///     }
+    ///
+    ///     for await sentence in sentenceStream {
+    ///         print("\"\(sentence)\"", terminator: ", ")
+    ///     }
+    ///     // Prints: "Hello, World!", "My name is Dimitri.", "", "Bye!"
+    ///
+    /// - Parameter transform: A mapping closure. `transform` accepts an iterator representing the original sequence as its parameter and returns a transformed value. Returning `nil` will stop the sequence early.
+    /// - Returns: An asynchronous sequence that contains, in order, elements produced by the `transform` closure.
+    @inlinable
+    public func iteratorMap<Transformed>(_ transform: @escaping (_ iterator: inout AsyncBufferedIterator<AsyncIterator>) async -> Transformed) -> AsyncIteratorMapSequence<Self, Transformed> {
+        AsyncIteratorMapSequence(self, transform: transform)
+    }
+    
+    /// Creates an asynchronous sequence that maps the given closure over an iterator for the sequence, which can itself accept multiple reads.
+    ///
+    /// When finished reading from the iterator, return your completed object, and the closure will be called again with an terator configured to continue where the first one finished.
+    ///
+    /// In this example, an asynchronous sequence of Strings encodes sentences by prefixing each word sequence with a number.
+    /// The number indicates how many words will be read and concatenated into a complete sentence.
+    ///
+    /// The closure provided to the `iteratorMap(_:)` first reads the first available string, interpreting it as a number.
+    /// Then, it will loop the specified number of times, accumulating those words into an array, that is finally assembled into a sentence.
+    ///
+    ///     let dataStream = ... // "2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "1", "Bye!"
+    ///
+    ///     let sentenceStream = dataStream.iteratorMap { iterator -> String? in
+    ///         guard var count = Int(try await iterator.next() ?? "") else {
+    ///             throw SentenceParsing.invalidWordCount
+    ///         }
+    ///
+    ///         var results: [String] = []
+    ///
+    ///         while count > 0, let next = try await iterator.next() {
+    ///             results.append(next)
+    ///             count -= 1
+    ///         }
+    ///
+    ///         guard count == 0 else {
+    ///             throw SentenceParsing.missingFinalWords
+    ///         }
+    ///
+    ///         return results.joined(separator: " ")
+    ///     }
+    ///
+    ///     for try await sentence in sentenceStream {
+    ///         print("\"\(sentence)\"", terminator: ", ")
+    ///     }
+    ///     // Prints: "Hello, World!", "My name is Dimitri.", "", "Bye!"
+    ///
+    /// - Parameter transform: A mapping closure. `transform` accepts an iterator representing the original sequence as its parameter and returns a transformed value. Returning `nil` will stop the sequence early, as will throwing an error.
+    /// - Returns: An asynchronous sequence that contains, in order, elements produced by the `transform` closure.
+    @inlinable
+    public func iteratorMap<Transformed>(_ transform: @escaping (_ iterator: inout AsyncBufferedIterator<AsyncIterator>) async throws -> Transformed) -> AsyncThrowingIteratorMapSequence<Self, Transformed> {
+        AsyncThrowingIteratorMapSequence(self, transform: transform)
+    }
+}
+
+/// An asynchronous sequence that maps the given closure over the asynchronous sequence’s elements by providing it with the base sequence's iterator to assemble multiple reads into a single transformed object.
+public struct AsyncIteratorMapSequence<Base : AsyncSequence, Transformed> {
+    @usableFromInline
+    let base: Base
+    
+    @usableFromInline
+    let transform: (_ iterator: inout AsyncBufferedIterator<Base.AsyncIterator>) async -> Transformed
+    
+    @usableFromInline
+    init(
+        _ base: Base,
+        transform: @escaping (_ iterator: inout AsyncBufferedIterator<Base.AsyncIterator>) async -> Transformed
+    ) {
+        self.base = base
+        self.transform = transform
+    }
+}
+
+extension AsyncIteratorMapSequence: AsyncSequence {
+    /// The type of element produced by this asynchronous sequence.
+    ///
+    /// The map sequence produces whatever type of element its transforming closure produces.
+    public typealias Element = Transformed
+    
+    /// The iterator that produces elements of the map sequence.
+    public struct AsyncIterator: AsyncIteratorProtocol {
+        @usableFromInline
+        var baseIterator: AsyncBufferedIterator<Base.AsyncIterator>
+        @usableFromInline
+        let transform: (_ iterator: inout AsyncBufferedIterator<Base.AsyncIterator>) async -> Transformed
+        
+        @usableFromInline
+        init(
+            _ baseIterator: AsyncBufferedIterator<Base.AsyncIterator>,
+            transform: @escaping (inout AsyncBufferedIterator<Base.AsyncIterator>) async -> Transformed
+        ) {
+            self.baseIterator = baseIterator
+            self.transform = transform
+        }
+        
+        /// Produces the next element in the map sequence.
+        ///
+        /// This iterator calls `next()` on its (wrapped) base iterator, and stores the result; if this call returns `nil`, `next()` returns `nil`. Otherwise, `next()` returns the result of calling the transforming closure on the received element.
+        @inlinable
+        public mutating func next() async rethrows -> Transformed? {
+            guard try await baseIterator.hasMoreData() else {
+                return nil
+            }
+            return await transform(&baseIterator)
+        }
+    }
+    
+    @inlinable
+    public func makeAsyncIterator() -> AsyncIterator {
+        AsyncIterator(AsyncBufferedIterator(base.makeAsyncIterator()), transform: transform)
+    }
+}
+
+public struct AsyncThrowingIteratorMapSequence<Base, Transformed> where Base : AsyncSequence {
+    @usableFromInline
+    let base: Base
+    
+    @usableFromInline
+    let transform: (_ iterator: inout AsyncBufferedIterator<Base.AsyncIterator>) async throws -> Transformed
+    
+    @usableFromInline
+    init(
+        _ base: Base,
+        transform: @escaping (_ iterator: inout AsyncBufferedIterator<Base.AsyncIterator>) async throws -> Transformed
+    ) {
+        self.base = base
+        self.transform = transform
+    }
+}
+
+extension AsyncThrowingIteratorMapSequence: AsyncSequence {
+    public typealias Element = Transformed
+    
+    public struct AsyncIterator: AsyncIteratorProtocol {
+        @usableFromInline
+        var baseIterator: AsyncBufferedIterator<Base.AsyncIterator>
+        
+        @usableFromInline
+        let transform: (_ iterator: inout AsyncBufferedIterator<Base.AsyncIterator>) async throws -> Transformed
+        
+        @usableFromInline
+        var encounteredError = false
+        
+        @usableFromInline
+        init(_ baseIterator: AsyncBufferedIterator<Base.AsyncIterator>, transform: @escaping (inout AsyncBufferedIterator<Base.AsyncIterator>) async throws -> Transformed) {
+            self.baseIterator = baseIterator
+            self.transform = transform
+        }
+        
+        /// Produces the next element in the map sequence.
+        ///
+        /// This iterator calls `next()` on its (wrapped) base iterator, and stores the result; if this call returns `nil`, `next()` returns `nil`. Otherwise, `next()` returns the result of calling the transforming closure on the received element. If calling the closure throws an error, the sequence ends and `next()` rethrows the error.
+        @inlinable
+        public mutating func next() async throws -> Transformed? {
+            guard !encounteredError, try await baseIterator.hasMoreData() else {
+                return nil
+            }
+            do {
+                return try await transform(&baseIterator)
+            } catch {
+                encounteredError = true
+                throw error
+            }
+        }
+    }
+    
+    @inlinable
+    public func makeAsyncIterator() -> AsyncIterator {
+        AsyncIterator(AsyncBufferedIterator(base.makeAsyncIterator()), transform: transform)
+    }
+}
+
+#endif
---
+++ Tests/AsyncSequenceReaderTests/AsyncIteratorMapSequenceTests.swift
@@ -0,0 +1,201 @@
+//
+//  AsyncIteratorMapSequenceTests.swift
+//  AsyncSequenceReader
+//
+//  Created by Dimitri Bouniol on 2021-11-16.
+//  Copyright © 2021 Mochi Development, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import AsyncSequenceReader
+
+final class AsyncIteratorMapSequenceTests: XCTestCase {
+    func testIteratorMapFromStream() async throws {
+        let testStream = AsyncStream<String> { continuation in
+            let data = ["2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "1", "Bye!"]
+            for value in data {
+                continuation.yield(value)
+            }
+            continuation.finish()
+        }
+        
+        let results = testStream.iteratorMap { iterator -> String? in
+            var count = Int(await iterator.next() ?? "")!
+            
+            var results: [String] = []
+            
+            while count > 0, let next = await iterator.next() {
+                results.append(next)
+                count -= 1
+            }
+            
+            return results.joined(separator: " ")
+        }
+        
+        var resultsIterator = results.makeAsyncIterator()
+        
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "Hello, World!")
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "My name is Dimitri.")
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "")
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "Bye!")
+    }
+    
+    func testIteratorMapFromInvalidStream() async throws {
+        struct LocalError: Error {}
+        
+        let testStream = AsyncStream<String> { continuation in
+            let data = ["2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "2", "Bye!"]
+            for value in data {
+                continuation.yield(value)
+            }
+            continuation.finish()
+        }
+        
+        let results = testStream.iteratorMap { iterator -> String? in
+            var count = Int(await iterator.next() ?? "")!
+            
+            var results: [String] = []
+            
+            while count > 0, let next = await iterator.next() {
+                results.append(next)
+                count -= 1
+            }
+            
+            guard count == 0 else {
+                throw LocalError()
+            }
+            
+            return results.joined(separator: " ")
+        }
+        
+        var resultsIterator = results.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "Hello, World!")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "My name is Dimitri.")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "")
+        do {
+            try await AsyncXCTAssertEqual(await resultsIterator.next(), "Bye!")
+            XCTFail("This should not succeed.")
+        } catch is LocalError {
+            
+        }
+    }
+    
+    func testIteratorMapFromTestSequence() async throws {
+        let testStream = TestSequence(base: ["2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "1", "Bye!"])
+        
+        let results = testStream.iteratorMap { iterator -> String? in
+            var count = Int(await iterator.next() ?? "")!
+            
+            var results: [String] = []
+            
+            while count > 0, let next = await iterator.next() {
+                results.append(next)
+                count -= 1
+            }
+            
+            return results.joined(separator: " ")
+        }
+        
+        var resultsIterator = results.makeAsyncIterator()
+        
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "Hello, World!")
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "My name is Dimitri.")
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "")
+        await AsyncXCTAssertEqual(await resultsIterator.next(), "Bye!")
+    }
+    
+    func testIteratorMapFromInvalidTestSequence() async throws {
+        struct LocalError: Error {}
+        
+        let testStream = TestSequence(base: ["2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "2", "Bye!"])
+        
+        let results = testStream.iteratorMap { iterator -> String? in
+            var count = Int(await iterator.next() ?? "")!
+            
+            var results: [String] = []
+            
+            while count > 0, let next = await iterator.next() {
+                results.append(next)
+                count -= 1
+            }
+            
+            guard count == 0 else {
+                throw LocalError()
+            }
+            
+            return results.joined(separator: " ")
+        }
+        
+        var resultsIterator = results.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "Hello, World!")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "My name is Dimitri.")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "")
+        do {
+            try await AsyncXCTAssertEqual(await resultsIterator.next(), "Bye!")
+            XCTFail("This should not succeed.")
+        } catch is LocalError {
+            
+        }
+    }
+    
+    func testIteratorMapFromThrowingTestSequence() async throws {
+        let testStream = ThrowingTestSequence(base: ["2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "1", "Bye!"])
+        
+        let results = testStream.iteratorMap { iterator -> String? in
+            var count = Int(try await iterator.next() ?? "")!
+            
+            var results: [String] = []
+            
+            while count > 0, let next = try await iterator.next() {
+                results.append(next)
+                count -= 1
+            }
+            
+            return results.joined(separator: " ")
+        }
+        
+        var resultsIterator = results.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "Hello, World!")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "My name is Dimitri.")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "Bye!")
+    }
+    
+    func testIteratorMapFromInvalidThrowingTestSequence() async throws {
+        struct LocalError: Error {}
+        
+        let testStream = ThrowingTestSequence(base: ["2", "Hello,", "World!", "4", "My", "name", "is", "Dimitri.", "0", "2", "Bye!"])
+        
+        let results = testStream.iteratorMap { iterator -> String? in
+            var count = Int(try await iterator.next() ?? "")!
+            
+            var results: [String] = []
+            
+            while count > 0, let next = try await iterator.next() {
+                results.append(next)
+                count -= 1
+            }
+            
+            guard count == 0 else {
+                throw LocalError()
+            }
+            
+            return results.joined(separator: " ")
+        }
+        
+        var resultsIterator = results.makeAsyncIterator()
+        
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "Hello, World!")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "My name is Dimitri.")
+        try await AsyncXCTAssertEqual(await resultsIterator.next(), "")
+        do {
+            try await AsyncXCTAssertEqual(await resultsIterator.next(), "Bye!")
+            XCTFail("This should not succeed.")
+        } catch is LocalError {
+            
+        }
+    }
+}
