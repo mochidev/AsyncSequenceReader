4220cb94cb0dd537b8bab48242eb3dd517363e00
Dimitri Bouniol <dimitribouniol@mochidev.com>
2024-05-19 13:56:10 -0700

    Updated error type to be resilient to API breakage

--- Sources/AsyncSequenceReader/AsyncSequenceReaderError.swift
+++ Sources/AsyncSequenceReader/AsyncSequenceReaderError.swift
@@ -3,13 +3,37 @@
 //  AsyncSequenceReader
 //
 //  Created by Dimitri Bouniol on 2021-11-17.
-//  Copyright © 2021 Mochi Development, Inc. All rights reserved.
+//  Copyright © 2021-24 Mochi Development, Inc. All rights reserved.
 //
 
 /// An Async Sequence Reader Error.
-public enum AsyncSequenceReaderError: Error {
+public struct AsyncSequenceReaderError: Error, Hashable, Sendable {
+    enum Code: Hashable, Sendable {
+        /// Not enough elements were available to read (ie. `actual < minimum`)
+        case insufficientElements(minimum: Int, actual: Int)
+        /// A termination was not found before the sequence ended (ie. `actual` will be less than or equal to `maximum`, indicating the amount of elements read)
+        case terminationNotFound(maximum: Int, actual: Int)
+    }
+    
+    let code: Code
+    
+    public var description: String {
+        "AsyncSequenceReaderError.\(String(describing: code))"
+    }
+    
+    static func ~= (lhs: Self, rhs: Error) -> Bool {
+        lhs == rhs as? AsyncSequenceReaderError
+    }
+}
+
+extension AsyncSequenceReaderError {
     /// Not enough elements were available to read (ie. `actual < minimum`)
-    case insufficientElements(minimum: Int, actual: Int)
+    public static func insufficientElements(minimum: Int, actual: Int) -> Self {
+        Self(code: .insufficientElements(minimum: minimum, actual: actual))
+    }
+    
     /// A termination was not found before the sequence ended (ie. `actual` will be less than or equal to `maximum`, indicating the amount of elements read)
-    case terminationNotFound(maximum: Int, actual: Int)
+    public static func terminationNotFound(maximum: Int, actual: Int) -> Self {
+        Self(code: .terminationNotFound(maximum: maximum, actual: actual))
+    }
 }
